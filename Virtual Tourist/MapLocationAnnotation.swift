35444c7ae03ef35437f0228da9dacd0e86adf292
Dylan <dylan@dylanedwards.net>
2016-08-23 18:29:22 -0700

    feat: Adds Location Annotation

---
+++ Virtual Tourist/MapLocationAnnotation.swift
@@ -0,0 +1,30 @@
+//
+//  StudentLocationAnnotation.swift
+//  On The Map
+//
+//  Created by Dylan Edwards on 6/16/16.
+//  Copyright © 2016 Slinging Pixels Media. All rights reserved.
+//
+
+import MapKit
+import Foundation
+
+final class StudentLocationAnnotation: NSObject, MKAnnotation, ReusableView {
+    internal let title: String?
+    internal let mediaURL: String
+    internal let locationName: String
+    internal let coordinate: CLLocationCoordinate2D
+    
+    internal var subtitle: String? {
+        return mediaURL
+    }
+    
+    init(title: String, mediaURL: String, locationName: String, coordinate: CLLocationCoordinate2D) {
+        self.title          = title
+        self.mediaURL       = mediaURL
+        self.locationName   = locationName
+        self.coordinate     = coordinate
+        
+        super.init()
+    }
+}
