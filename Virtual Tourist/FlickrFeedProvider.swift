c408e530ea71a18cc0281e7d6ffb558f4800c637
Dylan <dylan@dylanedwards.net>
2016-09-19 10:45:55 -0700

    feat: Adds class for Flickr feed

---
+++ Virtual Tourist/FlickrFeedProvider.swift
@@ -0,0 +1,13 @@
+//
+//  FlickrFeedProvider.swift
+//  Virtual Tourist
+//
+//  Created by Dylan Edwards on 9/19/16.
+//  Copyright © 2016 Slinging Pixels Media. All rights reserved.
+//
+
+import Foundation
+
+final class FlickrFeedProvider: NSObject {
+    
+}
